b653ebf693a3d7d4743511899972705b021a2f6e
zlqGitHub <1509874021@qq.com>
2018-10-24 09:13:40 +0800

    反转整数算法

--- easy/ReverseInteger/src/reverseIntger/ReverseIntger.java
+++ easy/ReverseInteger/src/reverseIntger/ReverseIntger.java
@@ -13,19 +13,20 @@ public class ReverseIntger {
 	
 	//反转整数算法
 	public int reverse(int x) {
-		int rev = 0;
-		
+		int rev = 0;   
+		System.out.println(Integer.MAX_VALUE);    //21474836487
+		System.out.println(Integer.MIN_VALUE);    //-2147483648
 		while(x != 0) {
-			int pop = x % 10;
-			x /= 10;
-			if(rev > Integer.MAX_VALUE /10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+			int pop = x % 10;   // 取得余数 
+			x /= 10;           //进行去位 
+			if(rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
 				return 0;
 			}
-			if(rev < Integer.MIN_VALUE /10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+			if(rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
 				return 0;
 			}
 			
-			rev = rev * 10 + pop;
+			rev = rev * 10 + pop;    //求出反转整数
 		}
 		
 		return rev; 
